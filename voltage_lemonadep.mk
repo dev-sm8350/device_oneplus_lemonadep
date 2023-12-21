d223a84909b0d1f6bc223289ba14adbecbc46f8f
TogoFire <togofire@mailfence.com>
2023-12-21 17:58:07 -0300

    lemonadep: voltage-ify
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: ff8bb9552f8dba9b2074712d45f4922caa0777ee

--- voltage_lemonadep.mk
+++ voltage_lemonadep.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021-2024 The LineageOS Project
+# Copyright (C) 2021-2024 The VoltageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -12,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from lemonadep device
 $(call inherit-product, device/oneplus/lemonadep/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Voltage stuff.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_lemonadep
+PRODUCT_NAME := voltage_lemonadep
 PRODUCT_DEVICE := lemonadep
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
@@ -33,3 +33,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceProduct=OnePlus9Pro \
     SystemDevice=OnePlus9Pro \
     SystemName=OnePlus9Pro
+
+# UDFPS animations
+EXTRA_UDFPS_ANIMATIONS := true
+
+# VoltageOS Stuffs
+VOLTAGE_BUILD_TYPE := OFFICIAL
