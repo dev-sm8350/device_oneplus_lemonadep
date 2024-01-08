d1bcee5a4ae8c720a22d3c1f7fefc6b7756a4d23
TogoFire <togofire@mailfence.com>
2024-01-08 17:33:54 -0300

    lemonadep: Set boot animation to 1920
    - ref: https://github.com/VoltageOS/vendor_voltage/blob/d15e4f0885cd35825b8cce1d0354c4e4042f382d/config/bootanimation.mk#L5
    https://github.com/VoltageOS/vendor_voltage/commit/b84fa4657be46863b69ccd3ed739538d0fc95bbc
    https://www.gsmarena.com/oneplus_9-10747.php
    https://www.gsmarena.com/oneplus_9_pro-10806.php
    
    Logically, it should be 2160, but it's not smooth.
    
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: I3b7d3ae04c3b05a08c2554ee906370436443c927

--- lineage_lemonadep.mk
+++ lineage_lemonadep.mk
@@ -23,6 +23,9 @@ PRODUCT_MODEL := LE2125
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
+# Boot animation resolution.
+TARGET_BOOT_ANIMATION_RES := 1920
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="OnePlus9Pro-user 14 UKQ1.230924.001 R.1d5a0be-1-4ffee release-keys" \
     BuildFingerprint=OnePlus/OnePlus9Pro/OnePlus9Pro:14/UKQ1.230924.001/R.1d5a0be-1-4ffee:user/release-keys \
